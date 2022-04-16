1673d3f860e95122f7afa7ffebab8d107ad8690a
Dor Skoler <dorskoler@gmail.com>
2022-04-16 22:10:56 +0300

    init

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,88 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.Navigation;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public void loginFunc(View view) {
+
+        String email = ((EditText) findViewById(R.id.email_login)).getText().toString().trim();
+        String password = ((EditText) findViewById(R.id.password_login)).getText().toString().trim();
+
+        System.out.println(email);
+        System.out.println(password);
+
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Logged in", Toast.LENGTH_LONG).show();
+                            Navigation.findNavController(view).navigate(R.id.action_blankFragment_to_blankFragment3);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Failed to login", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+    }
+
+    public void registerFunc(View view) {
+
+        String email = ((EditText) findViewById(R.id.email_register)).getText().toString().trim();
+        String password = ((EditText) findViewById(R.id.password_register)).getText().toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Registered Successful", Toast.LENGTH_LONG).show();
+                            Navigation.findNavController(view).navigate(R.id.action_blankFragment2_to_blankFragment4);
+                        } else {
+                            Toast.makeText(MainActivity.this, "Failed to register", Toast.LENGTH_LONG).show();
+                        }
+
+                    }
+                });
+    }
+
+    public String getUserName() {
+        return Objects.requireNonNull(mAuth.getCurrentUser()).getEmail();
+    }
+}
